dba131367bbac65538bfd694754db5522ef11d9c
Jean Cruz <cruzjean52@gmail.com>
2020-12-30 23:31:34 -0500

    Cambio de titulo por Laravel Framework

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel Framework
                 </div>
 
                 <div class="links">
